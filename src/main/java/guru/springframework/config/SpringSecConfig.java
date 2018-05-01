b3efa86d498e214c227b69c69a77efacae6f6692
giventadevelop <giventa.develop@gmail.com>
2018-05-01 19:46:47 -0400

    template security settings

--- src/main/java/guru/springframework/config/SpringSecConfig.java
+++ src/main/java/guru/springframework/config/SpringSecConfig.java
@@ -1,17 +1,23 @@
 package guru.springframework.config;
 
+import javax.sql.DataSource;
+
 import org.jasypt.springsecurity3.authentication.encoding.PasswordEncoder;
 import org.jasypt.util.password.StrongPasswordEncryptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
 
 @Configuration
 public class SpringSecConfig extends WebSecurityConfigurerAdapter {
@@ -20,6 +26,12 @@ public class SpringSecConfig extends WebSecurityConfigurerAdapter {
 	 private CustomAuthenticationProvider customAuthenticationProvider;
 	 @Autowired
 	 private UserDetailsService userDetailsService;
+	 
+	 @Autowired
+		CustomizeLogoutSuccessHandler customizeLogoutSuccessHandler;
+	 
+	 @Autowired
+	 DataSource dataSource;
 
     private AuthenticationProvider authenticationProvider;
 
@@ -58,21 +70,52 @@ public class SpringSecConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity httpSecurity) throws Exception {
     	//set access to all pages including session time out page where session time out is set in the application.properties page
            httpSecurity
-                .authorizeRequests().antMatchers("/","/products","/product/show/*","/session","/console/*","/h2-console/**").permitAll()
+                .authorizeRequests().antMatchers("/","/products","/product/show/*","/session","/logout","/console/*","/h2-console/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin().loginPage("/login").permitAll()
                 .and()
-                .logout().permitAll();
+               // .logout().logoutSuccessUrl("/login").permitAll().and()
+                //.rememberMe().rememberMeParameter("remember-me").rememberMeCookieName("remember-me").tokenValiditySeconds(86400).alwaysRemember(true);
+                //.useSecureCookie(true);
+                .rememberMe().rememberMeParameter("remember-me").tokenValiditySeconds(86400).userDetailsService(userDetailsService).tokenRepository(tokenRepository());
            //delete cookies so it won't get forwarded to session out page
-           httpSecurity.logout().deleteCookies("auth_code", "JSESSIONID").invalidateHttpSession(true);                          
-
+           //httpSecurity.logout().logoutSuccessUrl("/login?logout").deleteCookies("auth_code", "JSESSIONID").invalidateHttpSession(true); 
+           //httpSecurity.logout().logoutSuccessUrl("/logout").deleteCookies("auth_code", "JSESSIONID").invalidateHttpSession(true);
+           httpSecurity.logout().logoutSuccessUrl("/logout").deleteCookies("auth_code", "JSESSIONID").logoutSuccessHandler(customizeLogoutSuccessHandler).permitAll();
+           
+          // httpSecurity.logout().invalidateHttpSession(false);
+          /* httpSecurity
+           .rememberMe()
+               .rememberMeCookieName("remember-me-cookie-name")
+               .tokenValiditySeconds(86400);*/
+         /*  httpSecurity
+           .rememberMe().userDetailsService(userDetailsService)
+               .tokenValiditySeconds(86400);*/
         httpSecurity.csrf().disable();
         httpSecurity.headers().frameOptions().disable();
-       httpSecurity.sessionManagement().invalidSessionUrl("/session");
+        httpSecurity.sessionManagement().invalidSessionUrl("/session");
     }
     
+    @Bean
+    public PersistentTokenRepository tokenRepository() {
+    	
+      JdbcTokenRepositoryImpl jdbcTokenRepositoryImpl=new JdbcTokenRepositoryImpl();
+      jdbcTokenRepositoryImpl.setDataSource(dataSource);
+      return jdbcTokenRepositoryImpl;
+    }
     
-
+    /*  @Bean
+    @Primary
+    private DataSource dataSource() {
+        return DataSourceBuilder
+            .create()
+            .username("sa")
+            .password("")
+            .url("jdbc:h2:mem:testdb")
+            .driverClassName("org.h2.Driver")
+            .build();
+    }
+*/
 
 }
--- src/main/java/guru/springframework/controllers/IndexController.java
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -13,13 +13,27 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController {
     @RequestMapping("/")
-    String index(){
+    String index(HttpServletRequest request,HttpServletResponse response){
+    	
+    	Cookie[] cookies = request.getCookies();
+    	 
+        // Delete all the cookies
+        if (cookies != null) {
+     
+            for (int i = 0; i < cookies.length; i++) {
+     
+                Cookie cookie = cookies[i];
+               
+                	System.out.println("cookie_getName_root "+cookie.getName());
+               
+            }
+        }
         return "index";
     }
     
     @RequestMapping("/session")
     String session(HttpServletRequest request,HttpServletResponse response){
-    	SecurityContextHolder.clearContext();
+    	
     	HttpSession session= request.getSession(false);
     	Cookie[] cookies = request.getCookies();
     	 
@@ -29,9 +43,13 @@ public class IndexController {
             for (int i = 0; i < cookies.length; i++) {
      
                 Cookie cookie = cookies[i];
+               
+               	System.out.println("cookie_getName_session "+cookie.getName());
+               
                 cookies[i].setValue(null);
                 cookies[i].setMaxAge(0);
                 response.addCookie(cookie);
+               
             }
         }
     	
@@ -40,5 +58,34 @@ public class IndexController {
             session.invalidate();
         }
         return "session";
+    }
+    
+    @RequestMapping("/logout")
+    String logout(HttpServletRequest request,HttpServletResponse response){
+    	
+    	HttpSession session= request.getSession(false);
+    	Cookie[] cookies = request.getCookies();
+    	 
+        // Delete all the cookies
+        if (cookies != null) {
+     
+            for (int i = 0; i < cookies.length; i++) {
+     
+                Cookie cookie = cookies[i];
+               
+               	System.out.println("cookie_getName_session "+cookie.getName());
+               
+                cookies[i].setValue(null);
+                cookies[i].setMaxAge(0);
+                response.addCookie(cookie);
+               
+            }
+        }
+    	
+        SecurityContextHolder.clearContext();
+        if(session != null) {
+            session.invalidate();
+        }
+        return "logout";
     }
 }
